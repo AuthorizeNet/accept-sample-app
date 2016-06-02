91ba2af51d35a83b9fd9cb6e9ce94b47157ec216
sapbasu <sapbasu@visa.com>
2016-06-02 22:50:20 +0530

    added background images

--- scripts/localscripts.js
+++ scripts/localscripts.js
@@ -4,7 +4,7 @@ jQuery(document).ready(function() {
     /*
         Fullscreen background
     */
-    $.backstretch("assets/img/backgrounds/1.jpg");
+    $.backstretch("scripts/1.jpg");
     
     /*
         Form validation
