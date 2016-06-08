4d21994d7caa3facab6ab78c1755edf590d53b94
aamat <aamat@visa.com>
2016-06-08 19:03:11 +0530

    added acceptjs files

---
+++ transactionCaller.php
@@ -0,0 +1,36 @@
+<?php
+
+$transReqJson=json_decode($_POST['request']);
+
+$transReqJson->createTransactionRequest->merchantAuthentication->name='48uDp4QBA';
+$transReqJson->createTransactionRequest->merchantAuthentication->transactionKey='947j5q7tBgmAS378';//getenv('transaction_key');
+$transReqJson->createTransactionRequest->transactionRequest->currencyCode='USD';
+
+//$url="https://apitest.authorize.net/xml/v1/request.api";
+
+$url="https://api.authorize.net/xml/v1/request.api";
+//$url="https://downloadvposCED.labwebapp.com/xml/v1/request.api";
+
+try{	//setting the curl parameters.
+        $ch = curl_init();
+        if (FALSE === $ch)
+        	throw new Exception('failed to initialize');
+        curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($transReqJson));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 300);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_PROXY, "http://internet.visa.com:80");
+        curl_setopt($ch, CURLOPT_DNS_USE_GLOBAL_CACHE, false );
+        $content = curl_exec($ch);
+        if (FALSE === $content)
+        	throw new Exception(curl_error($ch), curl_errno($ch));
+        curl_close($ch);
+		echo $content;
+    }catch(Exception $e) {
+    	trigger_error(sprintf('Curl failed with error #%d: %s', $e->getCode(), $e->getMessage()), E_USER_ERROR);
+	}
+
+?>
