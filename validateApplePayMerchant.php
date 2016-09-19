a637bbcb5c7414e76715b14a249370a4a322d953
Brian McManus <brianmc@users.noreply.github.com>
2016-09-18 22:56:12 -0700

    Update validateApplePayMerchant.php

--- validateApplePayMerchant.php
+++ validateApplePayMerchant.php
@@ -7,9 +7,11 @@ ini_set("error_log", "/tmp/php-error.log");
 // Validation URL is passed in the request
 $validationUrl=$_POST['validationUrl'];
 
-echo $validationUrl;
+print $validationUrl;
 
 // JSON Payload 
 $validationPayload = '{"merchantIdentifier":"merchant.authorize.net.test.dev15","domainName":"applepay-sample.azurewebsites.net","displayName":"MyStore"}';
+
+print $validationPayload;
 
 ?>
