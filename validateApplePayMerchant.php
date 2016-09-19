ab40761cb33481355a12e33bcce6c919938aa3a4
Brian McManus <brianmc@users.noreply.github.com>
2016-09-18 23:02:28 -0700

    Update validateApplePayMerchant.php

--- validateApplePayMerchant.php
+++ validateApplePayMerchant.php
@@ -7,11 +7,30 @@ ini_set("error_log", "/tmp/php-error.log");
 // Validation URL is passed in the request
 $validationUrl=$_POST['validationUrl'];
 
-print $validationUrl;
 
 // JSON Payload 
 $validationPayload = '{"merchantIdentifier":"merchant.authorize.net.test.dev15","domainName":"applepay-sample.azurewebsites.net","displayName":"MyStore"}';
 
-print $validationPayload;
+try{	//setting the curl parameters.
+        $ch = curl_init();
+        if (FALSE === $ch)
+        	throw new Exception('failed to initialize');
+        curl_setopt($ch, CURLOPT_URL, $validationUrl);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $validationPayload);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 300);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_DNS_USE_GLOBAL_CACHE, false );
+        $content = curl_exec($ch);
+        $response = new SimpleXMLElement($content);
+        if (FALSE === $content)
+        	throw new Exception(curl_error($ch), curl_errno($ch));
+        curl_close($ch);
+        print $content;
+    }catch(Exception $e) {
+    	trigger_error(sprintf('Curl failed with error #%d: %s', $e->getCode(), $e->getMessage()), E_USER_ERROR);
+	}
 
 ?>
