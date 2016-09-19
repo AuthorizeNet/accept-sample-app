6e64a8a2e185992180b4bda8fa983e6072a181fd
Brian McManus <brianmc@users.noreply.github.com>
2016-09-18 22:54:16 -0700

    Update validateApplePayMerchant.php

--- validateApplePayMerchant.php
+++ validateApplePayMerchant.php
@@ -12,29 +12,4 @@ echo $validationUrl;
 // JSON Payload 
 $validationPayload = '{"merchantIdentifier":"merchant.authorize.net.test.dev15","domainName":"applepay-sample.azurewebsites.net","displayName":"MyStore"}';
 
-try{	//setting the curl parameters.
-        $ch = curl_init();
-        if (FALSE === $ch)
-        	throw new Exception('failed to initialize');
-        curl_setopt($ch, CURLOPT_URL, $validationUrl);
-		    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $validationPayload);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 300);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($ch, CURLOPT_DNS_USE_GLOBAL_CACHE, false );
-        $content = curl_exec($ch);
-        if (FALSE === $content)
-        	throw new Exception(curl_error($ch), curl_errno($ch));
-        curl_close($ch);
-		
-		// $content is the Apple Response, it should be a merchant session object
-		// but may need to do some manipulation here
-	echo $content;
-	
-    }catch(Exception $e) {
-    	trigger_error(sprintf('Curl failed with error #%d: %s', $e->getCode(), $e->getMessage()), E_USER_ERROR);
-	}
-
 ?>
